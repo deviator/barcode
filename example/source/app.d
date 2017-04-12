7affaeb7cd67d67b1ad3d25ae9877b0a330a72c9
deviator <code.viator@gmail.com>
2017-04-13 01:31:54 +0300

    rework

--- example/source/app.d
+++ example/source/app.d
@@ -4,57 +4,68 @@ import std.getopt;
 
 import barcode;
 
-enum Code
+class ListEncoder : BarCodeEncoder
 {
-    qr,
-    code128,
-    code39,
-    ean13
+    BarCodeEncoder[] list;
+    size_t cur;
+
+    this(BarCodeEncoder[] lst...)
+    { list = lst; }
+
+    BarCode encode(string str)
+    { return list[cur++%$].encode(str); }
 }
 
 int main(string[] args)
 {
-    if (args.length < 3)
+    if (args.length < 2)
     {
         import std.traits;
-        stderr.writefln("use: gen [-o<FILE>] -c<CODE> <STRING>\n<CODE>: %s", [EnumMembers!Code]);
+        stderr.writefln("use: gen [-o<FILE>] [--qr] [--code128] [--code39] [--ean13] <STRING>");
         return 1;
     }
 
     string output = "output.svg";
-    Code code = Code.qr;
+    bool useQr, useCode128, useCode39, useEAN13;
 
     getopt(args,
             "output|o", &output,
-            "code|c", &code,
+            "qr", &useQr,
+            "code128", &useCode128,
+            "code39", &useCode39,
+            "ean13", &useEAN13
             );
+
+    if (!(useQr || useCode128 || useCode39 || useEAN13))
+        useQr = true;
 
     string str = args[1..$].join(" ");
 
     writeln("output: ", output);
-    writeln("  code: ", code);
+    writefln(" codes: Qr %s, Code128 %s, Code39 %s, EAN13 %s",
+                      useQr, useCode128, useCode39, useEAN13);
     writeln("  data: ", str);
 
-    BarCodeEncoder enc;
-
     import barcode.qr : ECL;
-    final switch (code)
-    {
-        case Code.qr: enc = new Qr(ECL.high); break;
-        case Code.code128: enc = new Code128; break;
-        case Code.code39: enc = new Code39; break;
-        case Code.ean13: enc = new EAN13; break;
-    }
-
-    auto f = File(output, "w");
+    auto enc = new ListEncoder(
+        (useQr ? cast(BarCodeEncoder[])[new Qr(ECL.high)] : []) ~
+        (useCode128 ? cast(BarCodeEncoder[])[new Code128] : []) ~
+        (useCode39 ? cast(BarCodeEncoder[])[new Code39] : []) ~
+        (useEAN13 ? cast(BarCodeEncoder[])[new EAN13] : [])
+    );
 
     auto bbcsd = new BaseBarCodeSvgDrawer;
     bbcsd.fixSizeMode = true;
     bbcsd.W = 400;
     bbcsd.H = 50;
-    if (code == Code.qr)
-        bbcsd.H = 400;
-    f.write(bbcsd.draw(enc.encode(str)));
+
+    foreach (i; 0 .. enc.list.length)
+    {
+        auto bc = enc.encode(str);
+        bbcsd.H = bc.type == "qrcode" ? 400 : 50;
+        auto f = File(bc.type ~ "_" ~ output, "w");
+        f.write(bbcsd.draw(bc));
+    }
 
     return 0;
 }
